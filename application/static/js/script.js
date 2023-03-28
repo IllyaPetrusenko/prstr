bbd587035b2f425fd4e221793d9e88d17f58f76b
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-03-28 17:41:28 +0300

    add new carousel and adaptivity to mobile

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,4 +1,15 @@
-  var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
-  });
+$(document).ready(function() {
+  $('.carousel-card:first-of-type').addClass('active');
+
+  setInterval(function() {
+    var activeCard = $('.carousel-card.active');
+    var nextCard = activeCard.next();
+
+    if (nextCard.length === 0) {
+      nextCard = $('.carousel-card:first-of-type');
+    }
+
+    activeCard.removeClass('active');
+    nextCard.addClass('active');
+  }, 3000);
+});
