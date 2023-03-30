4dec206d562f2ec9d78a533a5979b5d575c94c43
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-03-30 16:20:32 +0300

    changed site design

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,15 +1,42 @@
-$(document).ready(function() {
-  $('.carousel-card:first-of-type').addClass('active');
+// Получение элементов страницы
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelectorAll('.modal-button');
+const close = document.querySelector('.close');
+const form = document.querySelector('.form');
+const phoneInput = document.querySelector('#phone');
 
-  setInterval(function() {
-    var activeCard = $('.carousel-card.active');
-    var nextCard = activeCard.next();
+// Открытие модального окна по клику на кнопку
+modalButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+});
 
-    if (nextCard.length === 0) {
-      nextCard = $('.carousel-card:first-of-type');
-    }
+// Закрытие модального окна по клику на крестик или вне модального окна
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
-    activeCard.removeClass('active');
-    nextCard.addClass('active');
-  }, 3000);
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// Проверка телефона при отправке формы
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Отмена действия по умолчанию (отправка формы)
+
+  const phone = phoneInput.value;
+
+  // Регулярное выражение для проверки телефона
+  const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+
+  if (!phoneRegex.test(phone)) {
+    alert('Некорректный номер телефона!');
+    return;
+  }
+
+  // Отправка формы
+  form.submit();
 });
