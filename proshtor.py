9f7a4e2cb2a0b7af59898329a86dc82180dfecac
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-04-06 16:09:54 +0300

    add callback form

--- proshtor.py
+++ proshtor.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+import asyncio
+from flask import Flask, render_template, make_response, request, jsonify
+import telegram
 
 app = Flask(__name__, template_folder='application/templates', static_folder='application/static')
 
@@ -14,8 +15,27 @@ def test():
     return render_template('index.html')
 
 
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_data = request.get_json()
+    name = json_data['name']
+    phone = json_data['phone']
+    comment = json_data['comment']
+
+    text = f'**** Нове звернення ****\nІмʼя: {name}\nТелефон: {phone}\nКоментарій: {comment}'
+
+    bot = telegram.Bot(token='6018637295:AAGd2neraCJFjGSpOIlX3YFIfqVnEQtbl0c')
+
+    async def main():
+        await bot.send_message(chat_id='-1001693907109', text=text)
+    asyncio.run(main())
+
+    return make_response('Created', 201)
+
+
 if __name__ == "__main__":
     # test
-    # app.run(host="127.0.0.1")
+    #app.run(host="127.0.0.1")
     # prod
     app.run(host="0.0.0.0")
+
