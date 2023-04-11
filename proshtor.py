5d137ba63ab498c7dc1dd886e74cbe3a382eab00
IllyaPetrusenko <illya.petrusenko@gmail.com>
2023-04-11 12:04:45 +0300

    add google conversions

--- proshtor.py
+++ proshtor.py
@@ -35,7 +35,7 @@ def callback():
 
 if __name__ == "__main__":
     # test
-    #app.run(host="127.0.0.1")
+    app.run(host="127.0.0.1")
     # prod
-    app.run(host="0.0.0.0")
+    # app.run(host="0.0.0.0")
 
